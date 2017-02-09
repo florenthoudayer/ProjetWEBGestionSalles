71514e7f963cf6bb3d7f3f66a8f61f37345695da
fhoudayer <fl.houdayer@gmail.com>
2017-02-09 11:45:53 +0100

    Ajout des datepicker

--- src/GestionSalleBundle/Form/ReservationType.php
+++ src/GestionSalleBundle/Form/ReservationType.php
@@ -3,6 +3,7 @@
 namespace GestionSalleBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,12 +14,16 @@ class ReservationType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('dateDebut', null, array('label' => 'Date de début' ,'required' => true))
-                ->add('dateFin', null, array('label' => 'Date de fin' ,'required' => true))
+        $builder->add('dateDebut', DateType::class, array('widget' => 'single_text',
+                        'label' => 'Date de début' ,'required' => true, 'html5' => false,
+                        'attr' => ['class' => 'js-datepicker']))
+                ->add('dateFin', DateType::class, array('widget' => 'single_text',
+                        'label' => 'Date de début' ,'required' => true, 'html5' => false,
+                        'attr' => ['class' => 'js-datepicker']))
                 ->add('idUtilisateur')
                 ->add('idSalles', null, array('label' => 'id salle' ,'required' => true))
                 ->add('idMatiere', null, array('label' => 'id matiere' ,'required' => true))
-                ->add('idFormation', null, array('label' => 'id formation' ,'required' => true))        ;
+                ->add('idFormation', null, array('label' => 'id formation' ,'required' => true));
     }
     
     /**
