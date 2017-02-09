31e7d19407b70fe60b4f4dbe48ac515ffbe67378
yboompook <nivekguerin@gmail.com>
2017-02-09 14:15:22 +0100

    maj bug merge

--- src/GestionSalleBundle/Form/ReservationType.php
+++ src/GestionSalleBundle/Form/ReservationType.php
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
 <?php
-
-namespace GestionSalleBundle\Form;
-
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-
-class ReservationType extends AbstractType
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('dateDebut')->add('dateFin')->add('idUser')->add('idSalles')->add('idMatiere')->add('idFormation')        ;
-    }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'GestionSalleBundle\Entity\Reservation'
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBlockPrefix()
-    {
-        return 'gestionsallebundle_reservation';
-    }
-
-
-}
-=======
-<?php
-
 namespace GestionSalleBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -60,7 +19,7 @@ class ReservationType extends AbstractType
                 ->add('dateFin', DateType::class, array('widget' => 'single_text',
                         'label' => 'Date de début' ,'required' => true, 'html5' => false,
                         'attr' => ['class' => 'js-datepicker']))
-                ->add('idUtilisateur')
+                ->add('idUser')
                 ->add('idSalles', null, array('label' => 'id salle' ,'required' => true))
                 ->add('idMatiere', null, array('label' => 'id matiere' ,'required' => true))
                 ->add('idFormation', null, array('label' => 'id formation' ,'required' => true));
@@ -86,4 +45,3 @@ class ReservationType extends AbstractType
 
 
 }
->>>>>>> origin/master
